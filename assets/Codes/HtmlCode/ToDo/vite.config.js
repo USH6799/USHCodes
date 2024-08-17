ced1f648f0f718c396dd42555513922726650ba7
USH6799 <sh.uttkarsh6799@gmail.com>
2024-08-17 21:52:16 +0530

    Uploading dist of TODO app

--- assets/Codes/HtmlCode/ToDo/vite.config.js
+++ assets/Codes/HtmlCode/ToDo/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/PORTFOLIO/assets/Codes/HtmlCode/ToDo",
+  base: "/USHCodes/assets/Codes/HtmlCode/ToDo",
   plugins: [react()],
 })
