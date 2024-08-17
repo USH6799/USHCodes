08adca75071d918976683a6f993506484be83a24
USH6799 <sh.uttkarsh6799@gmail.com>
2024-08-17 21:56:30 +0530

    Update vite.config.js

--- assets/Codes/HtmlCode/ToDo/vite.config.js
+++ assets/Codes/HtmlCode/ToDo/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/USHCodes/assets/Codes/HtmlCode/ToDo",
+  base: "/",
   plugins: [react()],
 })
